17698e785de77da7c8dc83a1edce75999505a6f0
William Khan <william.khan55@gmail.com>
2018-06-07 20:08:00 -0500

    Initial Commit

---
+++ XcodeC++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  XcodeC++
+//
+//  Created by William Khan on 6/7/18.
+//  Copyright © 2018 William Khan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
